17b2e1d756ab04215cc88fce17c24452ddae76e8
alamin-sde <ai.alamin.sde@gmail.com>
2025-09-07 16:21:36 +0530

    generateToken function created

---
+++ src/middleware/generateToken.ts
@@ -0,0 +1,6 @@
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
+export const generateToken=(userId:string)=>{
+  return jwt.sign({userId},process.env.JWT_SECRET as string,{expiresIn:'7d'})
+}
