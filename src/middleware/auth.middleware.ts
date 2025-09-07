d8885f8535dfce04eece15e8bdf2ec2ea3e27a17
alamin-sde <ai.alamin.sde@gmail.com>
2025-09-07 15:44:03 +0530

    auth.middleware function created

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken"
+import User from "../models/user.model"
+import { DecodedTokenType } from "../types/token.decoded.type";
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+        return res.status(401).json({ error: "Unauthorized" })
+    }
+
+    const decoded: DecodedTokenType = jwt.verify(token, process.env.JWT_SECRET as string) as unknown as DecodedTokenType
+    const user = await User.findOne({ _id: decoded.userId })
+    if (!user) {
+        return res.status(401).json({ error: "Unauthorized" })
+    }
+    next()
+}
