9b0799ee96bf06a7e43b75116453ac9702c4a0d1
alamin-sde <ai.alamin.sde@gmail.com>
2025-09-20 12:19:43 +0530

    addMessage and getContext added to chat.schema type

---
+++ src/types/chat.schema.type.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { MessageDataType } from "./message.type";
+
+export interface IChatSchemaType extends Document{
+    userId:mongoose.Schema.Types.ObjectId,
+    sessionId:string,
+    title:string,
+    messages:MessageDataType[],
+    isActive?:boolean,
+    tags?:string[],
+    summary?:string,
+    createdAt:Date,
+    updatedAt:Date,
+    addMessage:(data:MessageDataType)=>void,
+    getContext:(data:number)=>MessageDataType
+}
