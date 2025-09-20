e886a314527161ea01a1a760596522ebe27c30c1
alamin-sde <ai.alamin.sde@gmail.com>
2025-09-20 12:21:07 +0530

    sendMessage modified

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,5 +1,9 @@
 import router, { Request, Response } from "express"
 import { validationResult } from "express-validator";
+import Chat from "../models/chat.model"
+import { generateUniqueId } from "../utils/generate-unique-id";
+import { IChatSchemaType } from "../types/chat.schema.type";
+import { MessageDataType } from "../types/message.type";
 export const getQuickReplies = (req: Request, res: Response) => {
   const quickReplies = [
     "Hello! How can you help me today?",
@@ -16,7 +20,6 @@ export const getQuickReplies = (req: Request, res: Response) => {
 }
 
 export const sendMessage = async (req: Request, res: Response) => {
-  console.log(req.body)
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     res.status(400).json({
@@ -24,5 +27,22 @@ export const sendMessage = async (req: Request, res: Response) => {
       details: errors.array()
     })
   }
+  const {message,sessionId}=req.body;
+  const userId = ( req as any).userId;
+  let chat = await Chat.findOne({userId,isActive:true,sessionId})
+  
+  if(!chat){
+    chat = new Chat({
+      userId,
+      sessionId:sessionId || generateUniqueId(),
+      title: message.length > 50 ? message.substring(0,50)+"...":message
+    })
+
+  }
+  const messageData = {role:'user',content:message} as MessageDataType;
+  chat.addMessage(messageData);
+  const context = chat.getContext(20);
+  
+
 
 }
